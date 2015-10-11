48e16527c57581139278c0ddf9be1a0e376b1a43
Sayan Malakshinov <xt.and.r@gmail.com>
2015-10-11 23:20:18 +0300

    +clob_table

--- types.sql
+++ types.sql
@@ -1,2 +1,4 @@
-create or replace type varchar2_table as table of varchar2(4000);
-create or replace type clob_table     as table of clob;
+create or replace type varchar2_table as table of varchar2(4000)
+/
+create or replace type clob_table     as table of clob
+/
